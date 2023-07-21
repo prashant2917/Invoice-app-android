4f9cd653daace06fa2497d5d0555d6585e5691d1
Prashant Jondhale <jondhaleprashant@gmail.com>
2023-07-21 21:28:51 +0530

    added activity

---
+++ app/src/main/java/com/pocket/invoiceapp/mainapp/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.pocket.invoiceapp.mainapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.pocket.invoiceapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
